b2d2f22dd52195056f3e4ff6aa1ee0f7f3e65f5a
andrew <andrewliu278@gmail.com>
2024-01-17 21:32:07 -0500

    update api target

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
@@ -11,7 +11,7 @@ export default defineConfig(({ command, mode }) => {
       host: true,
       proxy: {
         "/api": {
-          target: "http://127.0.0.1:3000",
+          target: "https://image-to-image-translation-service.onrender.com:3000",
           changeOrigin: true,
         },
       },
