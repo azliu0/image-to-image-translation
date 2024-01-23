2aaf11f245505a791d909632a5ac160cd88fe207
andrew <andrewliu278@gmail.com>
2024-01-23 02:27:35 -0500

    modelbit config files

---
+++ deployment/source.py
@@ -0,0 +1,48 @@
+import numpy as np
+import server.pix2pix.modules.model_loader as model_loader
+from server.pix2pix.modules.pipeline.pipeline import generate
+from PIL import Image
+from transformers import CLIPTokenizer
+import boto3
+from server.utils.s3 import s3_to_pil, pil_to_s3
+
+DEVICE = "cpu"
+
+tokenizer = CLIPTokenizer(
+    "data/tokenizer_vocab.json", merges_file="data/tokenizer_merges.txt"
+)
+
+MODEL_PATH = "data/model.ckpt"
+
+models = model_loader.preload_models_from_standard_weights(MODEL_PATH, DEVICE)
+
+sampler = "ddpm"
+
+
+def inference_pix2pix_full_no_cfg_no_ddim_modelbit(opts):
+    try:
+        image = s3_to_pil()
+    except:
+        return "error uploading image"
+
+    output_image = generate(
+        prompt=opts["prompt"],
+        uncond_prompt=opts["negativePrompt"],
+        input_image=image,
+        strength=opts["temperature"],
+        do_cfg=True,
+        cfg_scale=opts["CFG"],
+        sampler_name=sampler,
+        n_inference_steps=opts["inferenceSteps"],
+        models=models,
+        device=DEVICE,
+        tokenizer=tokenizer,
+    )
+
+    output_image = output_image.astype(np.uint8)
+    output_image = Image.fromarray(output_image)
+
+    try:
+        pil_to_s3(output_image)
+    except:
+        return "error saving image"
