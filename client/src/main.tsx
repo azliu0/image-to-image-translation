b227a39cf0f3cb5f45ab111d02a0c0082b364bf2
Andrew Liu <andrewliu278@gmail.com>
2024-04-07 05:32:45 -0400

    default light mode \o/

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -18,7 +18,7 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
-    <MantineProvider theme={theme} defaultColorScheme="dark">
+    <MantineProvider theme={theme} defaultColorScheme="light">
       <Notifications />
       <Router>
         <Routes>
